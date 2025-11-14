22672d16107c9315fbaac2b59d8688518d800d8c
Finneyd16 <lavisco16@gmail.com>
2025-11-14 18:13:45 +0100

    feat: finished working on the bulk order product page

--- project/src/Pages/Ordernow.jsx
+++ project/src/Pages/Ordernow.jsx
@@ -3,12 +3,20 @@ import P35 from "../assets/P35.png";
 import { useParams } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 import { useState } from "react";
+import P36 from "../assets/P36.png";
+import P37 from "../assets/P37.png";
+import P38 from "../assets/P38.png";
+import P39 from "../assets/P39.png";
+import P40 from "../assets/P40.png";
+import P41 from "../assets/P41.png";
+import P42 from "../assets/P42.png";
+import ProductGrid from "../Components/Products";
 
 const Ordernow = () => {
   const { id } = useParams();
 
   const [quantity, setQuantity] = useState(1);
-  
+
   const increaseQty = () => setQuantity(quantity + 1);
   const decreaseQty = () => quantity > 1 && setQuantity(quantity - 1);
 
@@ -176,6 +184,294 @@ const Ordernow = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="pt-3 col-sm-12 col-md-8 col-lg-8">
+              <strong>Customer Reviews</strong>
+              <p
+                className="pt-3 small"
+                style={{ fontWeight: "bold", fontSize: "12px" }}
+              >
+                77 Reviews
+              </p>
+              <div className=" small">
+                <FaStar style={{ color: "#F58634" }} />
+                <FaStar style={{ color: "#F58634" }} />
+                <FaStar style={{ color: "#F58634" }} />
+                <FaStar style={{ color: "#F58634" }} />
+                <FaStar style={{ color: "#F58634" }} />
+              </div>
+              <img
+                style={{ height: "150px" }}
+                className="pt-1"
+                src={P36}
+                alt=""
+              />
+            </div>
+            <div className="pt-3 col-sm-12 col-md-4 col-lg-4">
+              <strong>How would you rate this?</strong>
+              <div className="small">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <p
+                className="pt-3 small"
+                style={{ fontWeight: "bold", fontSize: "12px" }}
+              >
+                Add a headline
+              </p>
+              <input
+                className="bulkholder1"
+                style={{
+                  width: "100%",
+                  height: "50px",
+                  borderColor: "#c4d1d073",
+                  border: "1px solid ",
+                  marginTop: "-5px",
+                  outline: "none",
+                  borderRadius: "5px",
+                }}
+                type="text"
+                placeholder="Write a summary of your review"
+              />
+              <p
+                className="mt-4 small"
+                style={{ fontWeight: "bold", fontSize: "12px" }}
+              >
+                Write a review
+              </p>
+              <input
+                className="bulkholder2"
+                style={{
+                  width: "100%",
+                  height: "110px",
+                  borderColor: "#c4d1d0a8",
+                  border: "1px solid",
+                  display: "flex",
+                  alignItems: "baseline",
+                  marginTop: "-5px",
+                  outline: "none",
+                  borderRadius: "5px",
+                }}
+                type="text"
+                placeholder="Tell us what do you think"
+              />
+              <button
+                className="mt-4 "
+                style={{
+                  padding: "10px 20px",
+                  borderRadius: "5px",
+                  backgroundColor: "#00A859",
+                  color: "white",
+                  border: "none",
+                  fontSize: "13px",
+                }}
+              >
+                Submit Review
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+          className="container mt-5 mb-5 pb-5 "
+        >
+          <div className="row">
+            <div style={{ gap: "15px" }} className="d-flex px-5">
+              <div
+                style={{
+                  border: "none",
+                  backgroundColor: " rgba(255, 255, 255, 0.01)",
+                  boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                  borderRadius: "9px",
+                }}
+                className=" bulkcard col-sm-12 col-md-4 col-lg-4 card "
+              >
+                <div className="card-body">
+                  <img src={P37} alt="" />
+                  <p className="mt-2">Sarah Taylor</p>
+                  <div style={{ marginTop: "-20px" }} className=" small">
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                  </div>
+                  <h6 className="mt-3 font-weight-bold">Organic Fruit Bites</h6>
+                  <p style={{ width: "95%" }} className="small">
+                    They're the perfect snack for my kids and I feel great
+                    knowing they're eating something healthy and natural.
+                  </p>
+                </div>
+              </div>
+              <div
+                style={{ borderRadius: "9px" }}
+                className="col-sm-12 col-md-4 col-lg-4 card "
+              >
+                <div className="card-body">
+                  <img src={P38} alt="" />
+                  <p className="mt-2">Meg Lanningr</p>
+                  <div style={{ marginTop: "-20px" }} className=" small">
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                  </div>
+                  <h6 className="mt-3 font-weight-bold">Organic Fruit Bites</h6>
+                  <p style={{ width: "95%" }} className="small">
+                    They're deliciously chewy and full of flavor. It's like
+                    eating a fresh mango without the mess.
+                  </p>
+                </div>
+              </div>
+              <div
+                style={{ borderRadius: "9px" }}
+                className="col-sm-12 col-md-4 col-lg-4 card "
+              >
+                <div className="card-body">
+                  <img src={P39} alt="" />
+                  <p className="mt-2">Alyssa Healy</p>
+                  <div style={{ marginTop: "-20px" }} className=" small">
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                  </div>
+                  <h6 className="mt-3 font-weight-bold">Crunchy Nut Mix</h6>
+                  <p style={{ width: "100%" }} className="small">
+                    I'm addicted to the Spicy Nut Mix! The blend of almonds,
+                    cashews, and walnuts with a hint of spice is just perfect.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 row">
+            <div style={{ gap: "15px" }} className="d-flex px-5">
+              <div
+                style={{ borderRadius: "9px" }}
+                className=" bulkcard col-sm-12 col-md-4 col-lg-4 card "
+              >
+                <div className="card-body">
+                  <img src={P40} alt="" />
+                  <p className="mt-2">Ellyse Perry</p>
+                  <div style={{ marginTop: "-20px" }} className=" small">
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                  </div>
+                  <h6 className="mt-3 font-weight-bold">Crunchy Nut Mix</h6>
+                  <p style={{ width: "95%" }} className="small">
+                    The Sweet & Salty Nut Mix is the best I've ever had. The
+                    nuts are roasted to perfection, and the balance of sweet.
+                  </p>
+                </div>
+              </div>
+              <div
+                style={{ borderRadius: "9px" }}
+                className="col-sm-12 col-md-4 col-lg-4 card "
+              >
+                <div className="card-body">
+                  <img src={P41} alt="" />
+                  <p className="mt-2">Beth Mooney</p>
+                  <div style={{ marginTop: "-20px" }} className=" small">
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                  </div>
+                  <h6 className="mt-3 font-weight-bold">Seed Power Bars</h6>
+                  <p style={{ width: "95%" }} className="small">
+                    The Original Seed Power Bar is a lifesaver for my busy
+                    mornings. It's packed with protein and fiber.
+                  </p>
+                </div>
+              </div>
+              <div
+                style={{ borderRadius: "9px" }}
+                className="col-sm-12 col-md-4 col-lg-4 card "
+              >
+                <div className="card-body">
+                  <img src={P42} alt="" />
+                  <p className="mt-2">Megan Schutt</p>
+                  <div style={{ marginTop: "-20px" }} className=" small">
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                    <FaStar style={{ color: "#F58634" }} />
+                  </div>
+                  <h6 className="mt-3 font-weight-bold">Seed Power Bars</h6>
+                  <p style={{ width: "100%" }} className="small">
+                    I tried the Chocolate Chip Seed Power Bar, and it exceeded
+                    my expectations. The combination is superb.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            className="mt-5 "
+            style={{
+              backgroundColor: "#00A859",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              padding: "10px 20px",
+            }}
+          >
+            Load More
+          </button>
+        </div>
+        <div style={{ marginTop: "8rem" }} className="page2 container">
+          <div className="row ">
+            <div className=" d-flex flex-column align-items-start">
+              <h1
+                style={{
+                  fontWeight: "800",
+                  fontSize: "450%",
+                  fontFamily: "MD Nichrome test",
+                  color: "#0F0B0B",
+                }}
+                className="col-sm-12 col-md-6 col-lg-8 "
+              >
+                Similar Products
+              </h1>
+              <div className="page2p ">
+                <p className=" text-muted col-sm-12 col-md-6 col-lg-5">
+                  Browse our most popular snacks and make your day more
+                  beautiful and glorious.
+                </p>
+                <button
+                  style={{
+                    padding: "10px 20px",
+                    borderRadius: "4px",
+                    color: "#00A859",
+                    backgroundColor: "transparent",
+                    borderColor: "#00A859 ",
+                    border: "1px solid",
+                  }}
+                  className="btn3"
+                >
+                  Browse All
+                </button>
+              </div>
+            </div>
+          </div>
+          <ProductGrid />
         </div>
       </>
     );
--- project/src/assets/index.jsx
+++ project/src/assets/index.jsx
@@ -32,9 +32,17 @@ import P31 from './P31.png';
 import P32 from './P32.png';
 import P33 from './P33.png';
 import P34 from './P34.png';
-import P35 from './P35.png';
+import P35 from './P35.png'; 
+import P36 from './P36.png'; 
+import P37 from './P37.png'; 
+import P38 from './P38.png'; 
+import P39 from './P39.png'; 
+import P40 from './P40.png'; 
+import P41 from './P41.png'; 
+import P42 from './P42.png'; 
 
 
 
 
-export {P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,P14,P15,P16,P17,P18,P19,P20,P21,P22,P23,P24,P25,P26,P27,P28,P29,P30,P31,P32,P33,P34,P35};
+
+export {P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,P14,P15,P16,P17,P18,P19,P20,P21,P22,P23,P24,P25,P26,P27,P28,P29,P30,P31,P32,P33,P34,P35,P36,P37,P38,P39,P40,P41,P42};
